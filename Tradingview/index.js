9aa6219819fcabefc4f3c48bf53878a6d4fdefe8
Ayush Kumar <ayushkumar4975@gmail.com>
2023-07-08 18:05:34 +0530

    committing for day 2

---
+++ Tradingview/index.js
@@ -0,0 +1,31 @@
+new TradingView.widget(
+    {
+        "autosize":true,
+        "symbol":"BINANCE:BTCUSDT",
+        "interval":"240",
+        "timezzone":"Etc/Utc",
+        "theme":"light",
+        "style":"1",
+        "locale":"en",
+        "toolbar_bg":"#f1f3f6",
+        "enable_publishing":false,
+        "withdaterages":true,
+        "hide_side_toolbar":false,
+        "allow_smybol_change":false,
+        "watchlist":[
+            "BINANCE:BTCUSDT",
+            "BINANCE:ETHUSDT"
+        ],
+        "details":true,
+        "hotlist":true,
+        "calender":true,
+        "studies":[
+            "STD;SMA"
+        ],
+        "container_id":"chart",
+        "show_popup_button":true,
+        "popup_width":"1000",
+        "popup_height":"650"
+
+    }
+);
